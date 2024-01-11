d3251ef4271a5afd4c332bbe8e65803a1dd8f1b3
Sam Mayer <samuel.a.mayer5.ctr@army.mil>
2024-01-11 10:39:10 -0600

    Refactor logic to get server PID

--- self-check.sh
+++ self-check.sh
@@ -6,13 +6,13 @@ npm install
 printf "Using dependency version: %s" "$(npm list @samayer12/npm-package-demo)"
 npm run dev > /dev/null 2>&1 & # Start server
 
-SERVER_PID=$! # Get process ID of the process we just started
+SERVER_PID=$(lsof -i :5173 | grep LISTEN | awk '{print $2}') # Get PID of process we just started
 
 sleep 5 # Wait for server to start
 
 curl localhost:5173 # Get webpage contents
 
-kill $SERVER_PID # Stop server
+kill "$SERVER_PID" # Stop server
 
 # Change dependency version
 printf "Changing dependency version to 0.0.9"
@@ -26,13 +26,13 @@ npm install
 printf "Using dependency version: %s" "$(npm list @samayer12/npm-package-demo)"
 npm run dev > /dev/null 2>&1 & # Start server
 
-SERVER_PID=$! # Get process ID of the process we just started
+SERVER_PID=$(lsof -i :5173 | grep LISTEN | awk '{print $2}') # Get PID of process we just started
 
 sleep 5 # Wait for server to start
 
 curl localhost:5173 # Get webpage contents
 
-kill $SERVER_PID # Stop server
+kill "$SERVER_PID" # Stop server
 
 mv package.json.bak package.json # Reset version
 git reset --hard HEAD # Reset to defaults, just to be sure
